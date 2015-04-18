daaf64c1f7447f790a5f8d17d57caac2560d9257
ex0ns <guillaume.tournigand@epfl.ch>
2015-04-19 01:23:41 +0200

    Fixing json

--- convert_data.rb
+++ convert_data.rb
@@ -1,18 +1,27 @@
 require 'json'
 
-file = File.read('data/ch.json')
-data = JSON.parse(file)
+cantons = File.read('server/public/data/ch-cantons-lakes.json')
+cantons = JSON.parse(cantons)
 
-data['objects']['municipalities']['geometries'].each do |muni|
-  muni['properties']['type'] = 'municipality'
-end
-
-data['objects']['cantons']['geometries'].each do |canton|
+cantons['objects']['cantons']['geometries'].each do |canton|
   canton['properties']['type'] = 'canton'
 end
 
-data['objects']['districts']['geometries'].each do |canton|
-  canton['properties']['type'] = 'district'
+File.write('server/public/data/ch-cantons-lakes.json', cantons.to_json)
+
+#municipalities = File.read('data/ch-cantons-lakes.json')
+#cantons = JSON.parse(cantons)
+
+#data['objects']['municipalities']['geometries'].each do |muni|
+#  muni['properties']['type'] = 'municipality'
+#end
+
+districts = File.read('server/public/data/ch-districts-lakes.json')
+districts = JSON.parse(districts)
+
+districts['objects']['districts']['geometries'].each do |canton|
+  districts['properties'] = Hash.new
+  districts['properties']['type'] = 'district'
 end
 
-File.write('data/ch-2.json', data.to_json)
+File.write('server/public/data/ch-districts-lakes.json', districts.to_json)
