28c4586b57a667b35fb0a12673f4584cea96279b
ex0ns <guillaume.tournigand@epfl.ch>
2015-04-18 17:33:12 +0200

    Routing and basic controller

--- server/app/controllers/cantons_controller.rb
+++ server/app/controllers/cantons_controller.rb
@@ -1,3 +1,13 @@
 class CantonsController < ApplicationController
 
+  def users_in_cantons
+    @canton = Canton.find_by_name(params[:name])
+    if !@canton.nil?
+      @users = User.find_by_canton_id(@canton.id)
+      render json: @users
+    else
+      render json: { status: 422 }
+    end
+  end
+
 end
--- server/app/controllers/communes_controller.rb
+++ server/app/controllers/communes_controller.rb
@@ -1,3 +1,13 @@
 class CommunesController < ApplicationController
 
+  def users_in_commune
+    @commune = Commune.find_by_name(params[:name])
+    if !@commune.nil?
+      @users = User.find_by_district(@commune.id)
+      render json: @users
+    else
+      render json: { status: 422 }
+    end
+  end
+
 end
