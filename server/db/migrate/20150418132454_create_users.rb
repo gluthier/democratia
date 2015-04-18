58645ec9d7295744ad2e0ad46b26b1bac1bcfe75
ex0ns <guillaume.tournigand@epfl.ch>
2015-04-18 16:15:21 +0200

    Migration and data regeneration

--- server/db/migrate/20150418132454_create_users.rb
+++ server/db/migrate/20150418132454_create_users.rb
@@ -5,6 +5,9 @@ class CreateUsers < ActiveRecord::Migration
       t.string :lastname
       t.string :email
       t.belongs_to :political_party
+      t.string :canton
+      t.string :district
+      t.string :city
       t.timestamps null: false
     end
   end
