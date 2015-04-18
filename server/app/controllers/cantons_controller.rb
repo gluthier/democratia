09ad53987d4d425701f468894eb0ce1e3f57575d
ex0ns <guillaume.tournigand@epfl.ch>
2015-04-18 22:55:43 +0200

    Fixing Cantons controller

--- server/app/controllers/cantons_controller.rb
+++ server/app/controllers/cantons_controller.rb
@@ -7,7 +7,7 @@ class CantonsController < ApplicationController
   def users_in_canton
     @canton = Canton.find_by_name(params[:name])
     if !@canton.nil?
-      @users = User.find_by_canton_id(@canton.id)
+      @users = User.where(canton: @canton)
       render json: @users
     else
       render json: { status: 422 }
