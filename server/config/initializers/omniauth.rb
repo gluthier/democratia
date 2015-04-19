86e3d18efdcd34b9c11f5c0cc7d8539e22c1d8fc
ex0ns <guillaume.tournigand@epfl.ch>
2015-04-19 03:11:09 +0200

    Another fix

--- server/config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, '621483531320934', '7ebc114be53a7e634596c3719e850c08'
-end
