20051619e76fd5e3c77982a8dde9838bb71aa7fb
ex0ns <guillaume.tournigand@epfl.ch>
2015-04-19 08:42:57 +0200

    Working map ! Need zoom and css

--- server/public/js/map.js
+++ server/public/js/map.js
@@ -16,31 +16,27 @@ svg.append("rect")
     .attr("width", width)
     .attr("height", height);
 
-var g = svg.append("g");
+var g = svg; //svg.append("g");
 
-d3.json("data/ch-cantons-lakes.json", function(error, ch) {
-  g.append("g")
-    .attr("id", "cantons")
-    .selectAll("path")
-    .data(topojson.feature(ch, ch.objects.cantons).features)
-    .enter().append("path")
-    .attr("d", path)
-    .on("click", clicked);
+d3.json("data/ch-cantons-lakes.json", function(error, ch){
 
 
+    var cantons = topojson.feature(ch, ch.objects.cantons);
+    var lakes = topojson.feature(ch, ch.objects.lakes);
 
-  g.append("path")
-      .datum(topojson.mesh(ch, ch.objects.cantons, function(a, b) { return a !== b; }))
-      .attr("id", "cantons-borders")
-      .attr("d", path);
+    svg.selectAll(".canton")
+        .data(cantons.features)
+        .enter()
+        .append("path")
+        .attr("class", "canton")
+        .attr("d", path)
+        .on("click", clicked);
 
+    svg.append("path")
+        .datum(lakes)
+        .attr("class", "lake")
+        .attr("d", path);
 
-  g.append("g")
-    .attr("class", "lakes")
-    .selectAll("path")
-    .data(topojson.feature(ch, ch.objects.lakes).features)
-    .enter().append("path")
-    .attr("d", path);
 });
 
 function clicked(d) {
@@ -52,39 +48,55 @@ function clicked(d) {
     y = centroid[1];
     k = 4;
 
-    if (!centered) { // We zoom on a canton
-        console.log("Test1");
-        d3.select(this).on("click", null);
-
+    if (!centered) {
+        //d3.select(this).on("click", null);
+        console.log("First time");
         d3.json("data/ch-districts-lakes.json", function (error, ch) {
-            g.append("g")
-                .attr("id", "districts")
-                .selectAll("path")
-                .data(topojson.feature(ch, ch.objects.districts).features)
-                .enter().append("path")
+            var districts = topojson.feature(ch, ch.objects.districts);
+            var lakes = topojson.feature(ch, ch.objects.lakes);
+
+            svg.selectAll(".district")
+                .data(districts.features)
+                .enter()
+                .append("path")
+                .attr("class", "district")
                 .attr("d", path)
-                //.attr("class", "municipality-boundaries")
                 .on("click", clicked);
+
+            svg.append("path")
+                .datum(lakes)
+                .attr("class", "lake")
+                .attr("d", path);
+
         });
 
 
     } else if(centered.properties.type === "canton" && d.properties.type === "district") { // We are focusing a District
-        console.log("data/" + centered.properties.abbr.toLowerCase() + "-municipalities-lakes.json");
-        d3.json("data/" + centered.properties.abbr.toLowerCase() + "-municipalities-lakes.json", function (error, ch) {
-            g.append("g")
-                .attr("id", centered.properties.name + "-municipalities")
-                .selectAll("path")
-                .data(topojson.feature(ch, ch.objects.municipalities).features)
-                .enter().append("path")
-                .attr("class", "municipality-boundaries")
-                .attr("id", "districts")
+        console.log("Second time");
+
+        d3.json("data/ch-municipalities-lakes.json", function (error, ch) {
+
+            var municipalities = topojson.feature(ch, ch.objects.municipalities);
+            var lakes = topojson.feature(ch, ch.objects.lakes);
+
+           svg.selectAll(".municipality")
+                .data(municipalities.features)
+                .enter()
+                .append("path")
+                .attr("class", "municipality")
                 .attr("d", path)
                 .on("click", clicked);
 
+           svg.append("path")
+                .datum(lakes)
+                .attr("class", "lake")
+                .attr("d", path);
+
+
         });
-    } /*else if(centered.properties.type === "district" && d.properties.type === "municipality") { // We are focusing a municipality
+    } else if(centered.properties.type === "district" && d.properties.type === "municipality") { // We are focusing a municipality
         console.log("test3");
-    }*/
+    }
 
     $.ajax({
       url: 'http://ex0ns.me:3000/cantons/' + d.properties.name + '/users',
@@ -99,14 +111,13 @@ function clicked(d) {
     cantonLevel = true;
 
   } else {
-      console.log("Else");
     x = width / 2;
     y = height / 2;
     k = 1;
     centered = null;
     cantonLevel = false;
     g.select("#districts").remove();
-    $('#personTable').html("<p id=\"ux_info\">Cliquez sur un canton pour afficher ces conseillers-ères.</p>");
+    $('#personTable').html("<p id=\"ux_info\">Cliquez sur un canton pour afficher ses conseillers-ères.</p>");
   }
 
 
@@ -114,7 +125,7 @@ function clicked(d) {
   g.selectAll("path")
       .classed("active", centered && function(d) { return d === centered; });
 
-  g.transition()
+  svg.transition()
       .duration(750)
       .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
       .style("stroke-width", 1.5 / k + "px");
