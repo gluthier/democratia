502136170b02c3e4c2333ffee2fd48d12f33d561
ex0ns <guillaume.tournigand@epfl.ch>
2015-04-19 16:41:27 +0200

    Final commit

--- server/app/controllers/municipalities_controller.rb
+++ server/app/controllers/municipalities_controller.rb
@@ -7,11 +7,11 @@ class MunicipalitiesController < ApplicationController
   def users_in_municipality
     @municipality = Municipality.find_by_name(params[:name])
     if !@municipality.nil?
-      @users = User.find_by_district(@municipality.id)
-      render json: @users
-    else
-      render json: { status: 422 }
+      @users = User.where(municipality_id: @municipality.id)
+      render layout: false
+      return
     end
+    render json: { status: 422 }
   end
 
 end
